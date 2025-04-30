18fd25a5f39546c8d67b9e71bb1340b7d5ca5015
Payam <payam85ar@gmail.com>
2025-04-30 10:12:56 +0330

    Write abstract class Product

---
+++ src/products/Product.java
@@ -0,0 +1,27 @@
+package products;
+
+public abstract class Product {
+    private String title;
+    private Double price;
+    private String id;
+
+    // Setters & Getters
+    public String getTitle() { return title; }
+    public Double getPrice() { return price; }
+    public String getId() { return id; }
+
+    // Constructor
+    public Product(String title, Double price) {
+        this.title = title;
+        this.price = price;
+        this.id = generateId();
+    }
+
+    // Methods
+    protected abstract String generateId();
+
+    @Override
+    public String toString() {
+        return "* ID: " + id + ", Title: " + title + ", Price: " + price;
+    }
+}
